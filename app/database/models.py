bd68f18a6bf2ca34bb49c8a0967c4d7d3a751193
Gur Sinai <gursin384@gmail.com>
2023-08-28 17:53:03 +0300

    Add survey to models.py

--- app/database/models.py
+++ app/database/models.py
@@ -9,3 +9,15 @@ class User():
     
     def __repr__(self) -> str:
         return f"{self.email}: {self.f_name} {self.s_name}. Share: {self.share_path}.\n{self.hashed_password}"
+
+class survey():
+        def __init__(self, id, name, start_date, end_date, owner_email) -> None:
+            self.id = id
+            self.name = name
+            self.start_date = start_date
+            self.end_date = end_date
+            self.owner_email = owner_email
+        
+        def __repr__(self) -> str:
+            return f"Survey number {self.id}: {self.name} from {self.start_date} to {self.end_date}.\nOwned by {self.owner_email}"
+        
