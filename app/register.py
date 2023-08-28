edc62f36dbcaa00bb16c86945aa4e89a4f74d0cc
yehonatancohen <yoncohenyon@gmail.com>
2023-08-28 16:20:04 +0300

    small changes

--- app/register.py
+++ app/register.py
@@ -2,15 +2,15 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_wtf.csrf import CSRFProtect
 from werkzeug.utils import secure_filename
-from forms import RegistrationForm
+from forms import RegistrationForm, LoginForm
 import database
-from helpers import captcha
+from helpers import captcha, key
 app = Flask(__name__)
 csrf = CSRFProtect(app)
 
 # Set up your database and import necessary functions/models
 
-@app.route('/register', methods=['POST'])
+@app.route('/register', methods=['POST', 'GET'])
 def register():
     form = RegistrationForm()
 
@@ -18,20 +18,45 @@ def register():
         # Get user input
         email = form.email.data
         password = form.password.data
-        image = form.image.data
+        
+        (salt, hashed_key, server_key) = key.generate_key()
 
-        # Save the user's email to the database (you may use SQLAlchemy or another ORM)
-        # Create a new user object and add it to the database
-
-        # Save the image file to a storage location
-        if image:
-            filename = secure_filename(image.filename)
-            image.save('path_to_your_storage/' + filename)
+        (user_share, server_share) = captcha.new_user()
+        
+        # send the user share to the user
+        # save the server share, salt and hash to the database 
 
         # Redirect to a success page or user profile page
         return redirect(url_for('registration_success'))
 
     return render_template('registration.html', form=form)
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        # Get user input
+        email = form.email.data
+        password = form.password.data
+        
+        (salt, hashed_key, server_key) = key.generate_key()
+
+        (user_share, server_share) = captcha.new_user()
+        
+        # send the user share to the user
+        # save the server share, salt and hash to the database 
+
+        # Redirect to a success page or user profile page
+        return redirect(url_for('registration_success'))
+
+    return render_template('registration.html', form=form)
+
+@app.route('/mail_check', methods=['POST', 'GET'])
+def mail_check(email):
+    pass
+    #check if email exits, if so, return server share, hash and salt
 
 @app.route('/registration-success')
 def registration_success():
