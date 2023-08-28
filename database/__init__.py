a99e9886112f583f1d40f29d62867b0845b9528e
Gur Sinai <gursin384@gmail.com>
2023-08-28 15:53:17 +0300

    Add user

--- database/__init__.py
+++ database/__init__.py
@@ -1,6 +1,19 @@
-import db_tables
-import models
+import database.db_tables as db_tables
+import database.models as models
+import database.db_queries as db_queries
+
+EMAIL_SIZE_LIMIT = 150
+F_NAME_SIZE_LIMIT = 100
+S_NAME_SIZE_LIMIT = 100
+
 
 def init_tables():
     db_tables.init_tables()
 
+def add_user(email, f_name, s_name, share2, password):
+    conn = db_tables.get_connection()
+    db_queries.__add_user__(conn, email, f_name, s_name, share2, password,
+                                  email_size_limit=EMAIL_SIZE_LIMIT, 
+                                  f_name_size_limit=F_NAME_SIZE_LIMIT,
+                                  s_name_size_limit=S_NAME_SIZE_LIMIT)
+    
