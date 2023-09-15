7eb52b0721e186681f6d67bb4359c28d7fca0765
yehonatancohen <coheyeho@gmail.com>
2023-09-15 14:47:32 +0300

    Added upload image function

--- app/blueprints/auth/static/auth/auth.js
+++ app/blueprints/auth/static/auth/auth.js
@@ -2,7 +2,10 @@ const emailButton = document.getElementById('continueButton');
 const wrapper = document.querySelector('.wrapper');
 const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]/;
 const email = document.getElementById('emailField');
+const imageUpload = document.getElementById('userShareImage');
 const loginLink = document.querySelector('.login-link');
+imageUpload.addEventListener('change', uploadImage);
+
 
 function goto(page, from)
 {
@@ -50,4 +53,35 @@ function validate_email()
             console.error('Fetch error:', error);
         });
     }
+}
+
+function uploadImage(event)
+{
+    var url = new URL(window.location.href).origin + "/submitshare";
+    const form = event.currentTarget;
+    var requestBody = new FormData(form);
+        
+        fetch(url, {
+            method: 'POST',
+            body: requestBody
+        })
+        .then(function(response) {
+            return response.text();
+        })
+        .then(function(data) {
+            data = JSON.parse(data);
+            succeed = data.succeed;
+            result = data.message;
+            if (succeed == false){
+                alert(result)
+            }
+            else
+            {
+                wrapper.classList.add('photo');
+            }
+        })
+        .catch(function(error) {
+            console.error('Fetch error:', error);
+        });
+    event.preventDefault();
 }
