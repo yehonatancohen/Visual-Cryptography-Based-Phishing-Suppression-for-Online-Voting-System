8722ac6af479907931de419eaf22e876e2e0a86c
Yuval <yuval1.maman@gmail.com>
2023-10-19 20:10:34 +0300

    results

--- app/blueprints/polls/static/polls/results.js
+++ app/blueprints/polls/static/polls/results.js
@@ -15,4 +15,4 @@ function openPoll(evt, name) {
     document.getElementById(name).style.display = "block";
     document.getElementById(name+'Chart').style.display = "flex";
     evt.currentTarget.className += " active";
-  }
+}
