59353ef2a0aae707db8c972e60a607313256a5e1
Gur Sinai <gursin384@gmail.com>
2023-10-30 19:05:07 +0200

    Bugfix-Returning duplicate surveys in git_surveys_related_to_user

--- app/database/users.py
+++ app/database/users.py
@@ -120,9 +120,19 @@ def get_surveys_related_to_user(user_email: str) -> list[dict]:
     from .surveys import get_user_surveys
     owned = get_user_surveys(user_email)
     result = list1
+
+
+
     for survey in owned:
-        if survey.id in [li["survey_id"] for li in list1]:
-            continue
+        # Check for duplicate surveys
+        existsInResult = False
+        for li in result:
+            if survey.id == li['survey_id']:
+                existsInResult = True
+                li['is_owner'] = True
+                break
+        if existsInResult: continue
+
         dict = {"survey_id":survey.id,
                 "survey_name":survey.name,
                 "start_date":survey.start_date,
