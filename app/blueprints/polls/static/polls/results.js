cc9b4124cd67e7bee521e4d44edaf4aa5f042275
yehonatancohen <coheyeho@gmail.com>
2023-11-21 14:11:37 +0200

    Added settings frontend logic

--- app/blueprints/polls/static/polls/results.js
+++ app/blueprints/polls/static/polls/results.js
@@ -21,3 +21,38 @@ function openPoll(evt, name) {
     document.getElementById(name+'Chart').style.display = "flex";
     evt.currentTarget.className += " active";
 }
+
+function settings(name){
+    settingVars = ['adduser','removeuser','changedate','deletesurvey'];
+    for (i = 0; i < settingVars.length; i++) {
+        settingVars[i].style.display = "none";
+        if(name == settingVars[i]){
+            settingVars[i].style.display = "none";
+        }
+    }
+}
+
+adduser.addEventListener('mouseover', function () {
+    // Change background color on hover
+    adduserInput.style.display = 'block';
+    removeuserInput.style.display = 'none';
+    adddateInput.style.display = 'none';
+    removedateInput.style.display = 'none';
+});
+
+removeuser.addEventListener('mouseover', function () {
+    // Change background color on hover
+    adduserInput.style.display = 'none';
+    removeuserInput.style.display = 'block';
+    adddateInput.style.display = 'none';
+    removedateInput.style.display = 'none';
+});
+
+
+changedate.addEventListener('mouseover', function () {
+    // Change background color on hover
+    adduserInput.style.display = 'none';
+    removeuserInput.style.display = 'none';
+    adddateInput.style.display = 'block';
+    removedateInput.style.display = 'block';
+});
